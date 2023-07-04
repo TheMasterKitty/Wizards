eb1048293579b1c8b55738c4473ca456ae80dce1
The Master Kitty <themasterkittyofcoding@gmail.com>
2023-07-04 19:37:33 -0400

    Created bases

---
+++ src/main/java/wizards/wizardtree/WizardTree.java
@@ -0,0 +1,17 @@
+package wizards.wizardtree;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WizardTree extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
